4079666302d02a6bf5921f531aa2e140ba30c505
albertjgonzalez <ajgonzalez1114@gmail.com>
2020-01-30 15:51:30 -0500

    add intro text to landing page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
+import { Wave } from 'react-animated-text';
 import './style.css';
+
+// const ExampleTwo = () => (
+//   <Random text="EXAMPLE TEXT" />
+// );
 
 class LandingPage extends Component {
 
-    state = { open: true }
+    state = {
+        open: true,
+        pauseText: true,
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ pauseText: false });
+        }, 1000);
+    }
+
     render() {
         return (
-            <div id="landingPage" >
+            <div id="landingPage">
+                <div id="landingAnimation">
+                    <Wave id="movingText" paused={this.state.pauseText} iterations="1" effect="verticalFadeIn" effectChange={2} speed="10" text="I am Albert J Gonzalez" />
+                    <Wave id="movingText" paused={this.state.pauseText} iterations="1" effect="verticalFadeIn" effectChange={4} speed="6" text="Welcome to my website" />
+                </div>
                 <div onClick={this.props.openPage} id="enterButton">
                     <p id="text">
                         Enter
                     </p>
                 </div>
+
             </div>
         );
     }
+
 }
 
 export default LandingPage;
